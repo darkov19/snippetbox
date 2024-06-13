e2ca1f42a7f0503e371d09217a4d97db8675d4d3
darkov19 <shastriv19@gmail.com>
2024-06-13 15:28:40 +0530

    Chpater 3.1 managing-configuration-settings

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"path/filepath"
 )
 
+type config struct {
+	addr      string
+	staticDir string
+}
+
 func main() {
+
+	var cfg config
+
+	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
+	flag.Parse()
 
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static")})
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir(cfg.staticDir)})
 
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
@@ -19,9 +31,9 @@ func main() {
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Print("starting server on :4000")
+	log.Printf("Starting server on http://localhost%s", cfg.addr)
 
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe(cfg.addr, mux)
 
 	log.Fatal(err)
 }
