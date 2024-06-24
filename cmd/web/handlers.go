805d2a32d6fc377d221c3fc26282ee0581cf9d0c
darkov19 <shastriv19@gmail.com>
2024-06-24 15:44:51 +0530

    05.02-template-actions-and-functions

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -20,10 +20,6 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	for _, snippet := range snippets {
-		fmt.Fprintf(w, "%+v\n", snippet)
-	}
-
 	templateFiles := []string{
 		"./ui/html/base.tmpl",
 		"./ui/html/partials/nav.tmpl",
@@ -37,7 +33,11 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = ts.ExecuteTemplate(w, "base", nil)
+	data := templateData{
+		Snippets: snippets,
+	}
+
+	err = ts.ExecuteTemplate(w, "base", data)
 
 	if err != nil {
 		app.serverError(w, r, err)
