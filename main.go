f23b02927235fb3c3569854871c6a66e8eb1809a
darkov19 <shastriv19@gmail.com>
2024-08-23 15:39:35 +0530

    Chapter 2.3. Routing requests

--- main.go
+++ main.go
@@ -6,14 +6,24 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World!"))
+	w.Write([]byte("Display the home page"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet"))
 }
 
 func main() {
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /", home)
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
 
 	log.Print("starting server on http://localhost:4000")
 
