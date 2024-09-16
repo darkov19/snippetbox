a30f4a6b56d80b8c2b5072fa387845ddd42110c3
darkov19 <shastriv19@gmail.com>
2024-09-16 15:48:04 +0530

    02.09-serving-static-files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,10 +3,46 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
 )
+
+type customFileSystem struct {
+	fs http.FileSystem
+}
+
+func (cfs customFileSystem) Open(path string) (http.File, error) {
+	f, err := cfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := cfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+
+			return nil, err
+		}
+	}
+
+	return f, nil
+}
 
 func main() {
 	mux := http.NewServeMux()
+
+	filerServer := http.FileServer(customFileSystem{http.Dir("./ui/static/")})
+
+	mux.Handle("GET /static/", http.StripPrefix("/static", filerServer))
+
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
