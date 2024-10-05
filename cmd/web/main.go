f9c3d24e91667b271aca4789ba8ed4e906810324
darkov19 <shastriv19@gmail.com>
2024-10-05 16:28:11 +0530

    09.06-connection-timeouts

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -112,6 +112,10 @@ func main() {
 		Handler:   app.routes(),
 		ErrorLog:  slog.NewLogLogger(logger.Handler(), slog.LevelError),
 		TLSConfig: tlsConfig,
+
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
 
 	logger.Info("starting server", "addr", srv.Addr)
